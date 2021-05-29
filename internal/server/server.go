bb67722bb4b17885bc8e236207a1479b8fab852f
bzp2010 <bzp20000818@gmail.com>
2021-05-29 13:50:34 +0800

    feat: connect to database

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,9 +2,15 @@ package server
 
 import (
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/gogf/gf/errors/gerror"
+	"github.com/gogf/gf/frame/g"
 	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type Options struct {
@@ -12,25 +18,69 @@ type Options struct {
 	Admin  bool
 }
 
+var publicMux *chi.Mux
+var database *gorm.DB
+
 func NewServer(o *Options) error {
+	// initialize database
+	err := initDatabase()
+	if err != nil {
+		return err
+	}
+
+	// start server
+	g.Log().Infof("Run WebVPN server: public: %t  admin: %t", o.Public, o.Admin)
 	if o.Public {
-		return initPublicServer()
+		err := initPublicServer()
+		if err != nil {
+			return gerror.Newf("public server start failed: %s", err.Error())
+		}
 	}
 
 	if o.Admin {
-		return initAdminServer()
+		err := initAdminServer()
+		if err != nil {
+			return gerror.Newf("admin server start failed: %s", err.Error())
+		}
 	}
 
 	return nil
 }
 
+func initDatabase() error {
+	g.Log().Info("connecting to database")
+
+	dsnURL := viper.GetString("dsn")
+	dsn := strings.Split(dsnURL, "://")
+
+	if dsn[0] != "mysql" {
+		return gerror.Newf("unsupported database software: %s", dsn[0])
+	}
+
+	var err error
+	switch dsn[0] {
+	case "mysql":
+	default:
+		database, err = gorm.Open(mysql.Open(dsn[1]), &gorm.Config{})
+		break
+	}
+
+	if err != nil {
+		return err
+	}
+
+	g.Log().Infof("connected to database: dsn=%s://%s", dsn[0], regexp.MustCompile(`(:).*(@)`).ReplaceAllString(dsn[1], ":****@"))
+
+	return nil
+}
+
 func initPublicServer() error {
-	r := chi.NewRouter()
-	r.Get("/*", Handler)
+	publicMux = chi.NewRouter()
+	publicMux.Get("/*", Handler)
 
 	hostAddr := viper.GetString("serve.public.host") + ":" + viper.GetString("serve.public.port")
 
-	return http.ListenAndServe(hostAddr, r)
+	return http.ListenAndServe(hostAddr, publicMux)
 }
 
 func initAdminServer() error {
