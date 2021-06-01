f6635a0f672de80ac5498828b4c890b1e2181d73
bzp2010 <bzp20000818@gmail.com>
2021-06-01 22:08:17 +0800

    feat: add migrate function

--- internal/server/server.go
+++ internal/server/server.go
@@ -18,71 +18,89 @@ type Options struct {
 	Admin  bool
 }
 
-var publicMux *chi.Mux
-var database *gorm.DB
-
-func NewServer(o *Options) error {
-	// initialize database
-	err := initDatabase()
-	if err != nil {
-		return err
-	}
-
-	// start server
-	g.Log().Infof("Run WebVPN server: public: %t  admin: %t", o.Public, o.Admin)
-	if o.Public {
-		err := initPublicServer()
-		if err != nil {
-			return gerror.Newf("public server start failed: %s", err.Error())
-		}
-	}
-
-	if o.Admin {
-		err := initAdminServer()
-		if err != nil {
-			return gerror.Newf("admin server start failed: %s", err.Error())
-		}
-	}
-
-	return nil
+type server struct {
+	options   *Options
+	publicMux *chi.Mux
+	adminMux  *chi.Mux
+	DB        *gorm.DB
 }
 
-func initDatabase() error {
+func NewServer(o *Options) (*server, error) {
+	g.Log().Infof("create webvpn server: public: %t  admin: %t", o.Public, o.Admin)
+
+	// create server
+	s := &server{options: o}
+
+	// initialize database
+	db, err := initDatabase()
+	if err != nil {
+		return nil, err
+	}
+	s.DB = db
+
+	// init server mux
+	if o.Public {
+		s.publicMux = initPublicServer()
+	}
+	if o.Admin {
+		s.adminMux = initAdminServer()
+	}
+
+	return s, nil
+}
+
+func initDatabase() (*gorm.DB, error) {
 	g.Log().Info("connecting to database")
 
 	dsnURL := viper.GetString("dsn")
 	dsn := strings.Split(dsnURL, "://")
 
 	if dsn[0] != "mysql" {
-		return gerror.Newf("unsupported database software: %s", dsn[0])
+		return nil, gerror.Newf("unsupported database engine: %s", dsn[0])
 	}
 
-	var err error
-	switch dsn[0] {
-	case "mysql":
-	default:
-		database, err = gorm.Open(mysql.Open(dsn[1]), &gorm.Config{})
-		break
-	}
-
+	db, err := gorm.Open(mysql.Open(dsn[1]), &gorm.Config{})
 	if err != nil {
-		return err
+		return nil, gerror.Newf("connect to database failed: %s", err.Error())
 	}
+
+	/*err = db.AutoMigrate(&model.Service{})
+	if err != nil {
+		return nil, err
+	}*/
 
 	g.Log().Infof("connected to database: dsn=%s://%s", dsn[0], regexp.MustCompile(`(:).*(@)`).ReplaceAllString(dsn[1], ":****@"))
 
+	return db, nil
+}
+
+func initPublicServer() *chi.Mux {
+	r := chi.NewRouter()
+	r.Get("/*", Handler)
+
+	return r
+}
+
+func initAdminServer() *chi.Mux {
 	return nil
 }
 
-func initPublicServer() error {
-	publicMux = chi.NewRouter()
-	publicMux.Get("/*", Handler)
+func (s *server) Start() error {
+	if s.options.Public {
+		hostAddr := viper.GetString("serve.public.host") + ":" + viper.GetString("serve.public.port")
+		err := http.ListenAndServe(hostAddr, s.publicMux)
+		if err != nil {
+			return gerror.Newf("public server start failed: %s", err.Error())
+		}
+	}
 
-	hostAddr := viper.GetString("serve.public.host") + ":" + viper.GetString("serve.public.port")
+	if s.options.Admin {
+		hostAddr := viper.GetString("serve.admin.host") + ":" + viper.GetString("serve.admin.port")
+		err := http.ListenAndServe(hostAddr, s.adminMux)
+		if err != nil {
+			return gerror.Newf("admin server start failed: %s", err.Error())
+		}
+	}
 
-	return http.ListenAndServe(hostAddr, publicMux)
-}
-
-func initAdminServer() error {
 	return nil
 }
